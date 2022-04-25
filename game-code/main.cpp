8d9fcb5bca5441fa91e496a38d7fd85284ac966d
fraander <57777918+fraander@users.noreply.github.com>
2022-04-25 11:30:03 -0400

    generate player hands, print player hands

--- game-code/main.cpp
+++ game-code/main.cpp
@@ -36,7 +36,7 @@ void generate_boss(string (&boss_hand)[3]) {
  * @param boss_hand
  * @return
  */
-bool attack_boss(string card_played, string (&boss_hand)[3]) {
+bool attack_boss(const string& card_played, string (&boss_hand)[3]) {
     for (int i=0; i < 3; i++) { // 3 for len boss_hand
         if (boss_hand[i] == card_played) {
             boss_hand[i] = "";
@@ -75,17 +75,47 @@ void print_boss_hand(string boss_hand[3]) {
     cout << endl;
 }
 
+/**
+ * Pick hand from possible hand for player
+ * @return
+ */
+ void generate_player(string (&player_hand)[5]) {
+    int temp_hand_index = rand_int(4); // pick from 1 of 4 possible hands
+
+    for (int i=0; i < 4; i++) {
+        player_hand[i] = possible_hands[temp_hand_index][i];
+    }
+ }
+
+ void print_player_hand(string player_hand[5]) {
+     for (int i=0; i < 5; i++) { // 5 for len player_hand
+         if (player_hand[i] == "") {
+             cout << "-";
+         } else {
+             cout << player_hand[i];
+         }
+     }
+     cout << endl;
+ }
+
 int main() {
     srand(time(0));
 
-    string boss_hand[3];
+    string boss_hand[3]; // create boss hand example
     generate_boss(boss_hand);
     print_boss_hand(boss_hand);
 
-    attack_boss("a", boss_hand);
+    attack_boss("a", boss_hand); // attack boss example
     attack_boss("a", boss_hand);
     attack_boss("a", boss_hand);
     print_boss_hand(boss_hand);
+    cout << boss_alive(boss_hand) << endl;
+
+    string player_hand[5]; // create player hand example
+    generate_player(player_hand);
+    print_player_hand(player_hand);
+
+    // TODO: play_card function--get a specific card from the hand (cin, will be button; use index [#]) and return the card
 
     return 0;
 }
